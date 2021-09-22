76661a086e3f30f85d15169fbd50ac7b905a18fc
Josip Josic <71074064+Josipjosic@users.noreply.github.com>
2021-09-22 20:31:06 +0200

    Start of the react project movise

---
+++ src/components/movies.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, release_date, vote_average ,original_language}) => (
+    <div className="movie"> 
+        <img src={IMG_API + poster_path} alt={title} />
+        <div className="movie-info">
+            <h3>{title}</h3>
+            <span>({release_date})</span>
+            <h5>Language:{original_language}</h5>
+            <span>{vote_average}</span>
+        </div>
+    </div>
+)
+
+export default Movie;
