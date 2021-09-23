a8ea02adc4e5612e898da5e789dc37d55c5bb891
Josip Josic <71074064+Josipjosic@users.noreply.github.com>
2021-09-23 15:35:04 +0200

    css editing

--- src/components/movies.js
+++ src/components/movies.js
@@ -4,12 +4,13 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const Movie = ({ title, poster_path, release_date, vote_average ,original_language}) => (
     <div className="movie"> 
+        <span className="movie-vote">{vote_average}</span>
         <img src={IMG_API + poster_path} alt={title} />
+        
+        
         <div className="movie-info">
-            <h3>{title}</h3>
-            <span>({release_date})</span>
-            <h5>Language:{original_language}</h5>
-            <span>{vote_average}</span>
+            <h3>{title} ({release_date})</h3>
+            <h5 className="movie-lang">Language: {original_language} </h5>
         </div>
     </div>
 )
